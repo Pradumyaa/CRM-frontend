05778f73960f5fa683aa2d0815347517b0f3de93
Pradumyaa <pradumya@getmaxsolutions.com>
2025-03-19 02:38:00 +0530

    Update DashboardContent.jsx

--- src/pages/dashboard/DashboardContent.jsx
+++ src/pages/dashboard/DashboardContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "../components/ui/card"
-import { Button } from "../components/ui/button";
+import { Button } from "../components/ui/Button";
 import { Clock, CalendarCheck, Briefcase } from "lucide-react";
 import DashboardGreeting from "./DashboardGreeting";
 
